4f1bdfe5ae85ac384d34bd8bd2702df4695c2918
melody-ste <melody_stephan@icloud.com>
2025-09-30 17:52:39 +0200

    install router + create navbar and home files

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,17 @@
+const Navbar = () => {
+  return (
+    <nav>
+      <ul>
+        <li>
+          <Link to="/">
+            Home
+          </Link>
+        </li>
+      </ul>
+      <ul>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
